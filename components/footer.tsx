1b478bba0116e9ff86627e538772d8ba1d927ccc
Valtteri <savonen.emppu@gmail.com>
2025-04-25 19:45:04 +0300

    added github link and changed link styling. New icon for mail

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,27 +1,36 @@
-import { Coffee, Inbox } from "lucide-react";
+import { Mail, Github } from "lucide-react";
 import Link from "next/link";
 
 export default function Footer() {
   return (
     <footer className="bg-white text-white py-8 w-full">
       <div className="container mx-auto px-4">
-        <div className="flex flex-col space-y-2 md:space-y-0 md:flex-row items-center justify-center space-x-4">
+        <div className="flex md:flex-row items-center justify-center space-x-4">
           <Link
             href="mailto:haalarikone@bittive.com"
-            className="flex items-center justify-center bg-green hover:bg-lime-700 text-white font-bold py-2 px-4 rounded-full transition duration-300 ease-in-out transform hover:scale-105 flex items-center"
+            className="text-black hover:text-green"
+            aria-label="Contact us"
           >
-            <Inbox className="mr-2" />
-            Ota meihin yhteyttä
+            <Mail className="" />
+          </Link>
+          <Link
+            href={"https://github.com/valtterisa/student-overall-app"}
+            target="_blank"
+            className="text-black hover:text-green"
+            aria-label="GitHub repository"
+          >
+            <Github className="" />
           </Link>
         </div>
         <p className="mt-4 text-center text-sm text-black">
           Made with 💖 by{" "}
-          <a
+          <Link
             href="https://bittive.com"
             className="font-semibold hover:underline"
+            target="_blank"
           >
             Bittive
-          </a>
+          </Link>
         </p>
       </div>
     </footer>
